d08d50cf1216dfe1bd01166f66d7ff0600c2a783
griskonis <60225096+griskonis@users.noreply.github.com>
2023-07-24 18:07:14 -0300

    Update WeatherForecastController.cs

--- API.Teste/Controllers/WeatherForecastController.cs
+++ API.Teste/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace API.Teste.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "FreezingMALA_FABIO_TESTE", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
